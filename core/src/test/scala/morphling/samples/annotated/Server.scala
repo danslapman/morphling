2c4cd4ef7a217ee64ebb4c335291361025d3c49e
Daniel Slapman <danslapman@gmail.com>
2019-06-10 00:29:39 +0300

    Rename annotated SSchema into ASchema
    Disable semanticdb due to strange bug

--- core/src/test/scala/morphling/samples/annotated/Server.scala
+++ core/src/test/scala/morphling/samples/annotated/Server.scala
@@ -2,7 +2,13 @@ package morphling.samples.annotated
 
 import cats.syntax.apply._
 import monocle.macros.GenLens
+import morphling.{HFix, HFunctor, SchemaF}
+import morphling.HEnvT._
+import morphling.HFix._
 import morphling.annotated.Schema._
+import morphling.Schema.{Schema => PlainSchema}
+import morphling.protocol.SType
+import morphling.protocol.SType.SSchema
 import morphling.protocol.annotated.SType._
 
 case class Server(host: String, port: Int)
@@ -10,11 +16,16 @@ object Server {
   val host = GenLens[Server](_.host)
   val port = GenLens[Server](_.port)
 
-  val schema: Schema[SSchema[Restriction, ?], Restriction, Server] = rec(
+  val schema: Schema[ASchema[Restriction, ?], Restriction, Server] = rec(
     (
-      required("host", sStr(NoRestr.asInstanceOf[Restriction]), host.asGetter),
-      required("port", sInt(Range(1, 65535).asInstanceOf[Restriction]), port.asGetter)
+      required("host", sStr(NoRestr: Restriction), host.asGetter),
+      required("port", sInt(Range(1, 65535): Restriction), port.asGetter)
     ).mapN(Server.apply),
-    NoRestr.asInstanceOf[Restriction]
+    NoRestr: Restriction
   )
+
+  val unSchema = //: HFix[SchemaF[PSSchema, ?[_], ?], Server] = //: PlainSchema[PSSchema, Server] =
+    HFix.forget[SchemaF[ASchema[Restriction, ?], ?[_], ?], Restriction]
+      .apply(schema)
+
 }
