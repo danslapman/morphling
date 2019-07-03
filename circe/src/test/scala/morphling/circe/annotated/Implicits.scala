89da1de22a10b2c726d659ad2c88ec5ef801d0b1
Daniel Slapman <danslapman@gmail.com>
2019-07-03 18:26:30 +0300

    Annotation-based validated decoder

--- circe/src/test/scala/morphling/circe/annotated/Implicits.scala
+++ circe/src/test/scala/morphling/circe/annotated/Implicits.scala
@@ -4,7 +4,6 @@ import cats._
 import io.circe.{AccumulatingDecoder, Decoder, Encoder}
 import morphling.circe.{FromJson, ToJson}
 import morphling.protocol.{SArrayT, SBoolT, SCharT, SDoubleT, SFloatT, SIntT, SLongT, SNullT, SStrT}
-import morphling.protocol.annotated._
 import morphling.protocol.annotated.SType.ASchema
 
 object Implicits {
@@ -31,15 +30,15 @@ object Implicits {
 
     val decoder = new (ASchema[A, ?] ~> Decoder) {
       def apply[I](s: ASchema[A, I]): Decoder[I] = s.unmutu match {
-        case _: SNullT[s.Inner]   => Decoder.decodeUnit
-        case _: SBoolT[s.Inner]   => Decoder.decodeBoolean
+        case _: SNullT[s.Inner]     => Decoder.decodeUnit
+        case _: SBoolT[s.Inner]     => Decoder.decodeBoolean
         case _: SIntT[s.Inner]      => Decoder.decodeInt
         case _: SLongT[s.Inner]     => Decoder.decodeLong
-        case _: SFloatT[s.Inner]   => Decoder.decodeFloat
-        case _: SDoubleT[s.Inner]  => Decoder.decodeDouble
-        case _: SCharT[s.Inner]    => Decoder.decodeChar
-        case _: SStrT[s.Inner]     => Decoder.decodeString
-        //case SArrayT(elem) => Decoder.decodeVector(elem.decoder)
+        case _: SFloatT[s.Inner]    => Decoder.decodeFloat
+        case _: SDoubleT[s.Inner]   => Decoder.decodeDouble
+        case _: SCharT[s.Inner]     => Decoder.decodeChar
+        case _: SStrT[s.Inner]      => Decoder.decodeString
+        case a: SArrayT[s.Inner, i] => Decoder.decodeVector(a.elem.decoder)
       }
     }
 
