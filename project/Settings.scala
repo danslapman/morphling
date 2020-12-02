0eeb4b6f0613ea48cc2613ef96a569f48b7606b4
Daniel Slapman <danslapman@gmail.com>
2020-12-02 19:23:29 +0300

    Update scala

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,8 +7,8 @@ object Settings {
   val common = Seq(
     organization := "danslapman",
     version := "2.5.2",
-    scalaVersion := "2.13.3",
-    crossScalaVersions := Seq("2.12.12", "2.13.3"),
+    scalaVersion := "2.13.4",
+    crossScalaVersions := Seq("2.12.12", "2.13.4"),
     scalacOptions ++= Seq(
       "-language:higherKinds,implicitConversions",
       "-Ywarn-unused:imports",
@@ -20,9 +20,9 @@ object Settings {
         case _ => Seq("-Ypartial-unification")
       }
     },
-    addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full),
+    addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.1" cross CrossVersion.full),
     addCompilerPlugin(scalafixSemanticdb),
-    scalafixDependencies in ThisBuild += "com.github.liancheng" %% "organize-imports" % "0.4.0",
+    scalafixDependencies in ThisBuild += "com.github.liancheng" %% "organize-imports" % "0.4.4",
     licenses += ("WTFPL", url("http://www.wtfpl.net")),
     bintrayOrganization := Some("danslapman"),
     bintrayReleaseOnPublish in ThisBuild := false
